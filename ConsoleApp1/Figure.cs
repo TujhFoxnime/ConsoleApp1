bd0321405c9451a00290b50fde29397e1b5cf7e9
EgorPushkar <113050829+TujhFoxnime@users.noreply.github.com>
2023-09-23 13:03:15 +0300

    figure.cs fixed

--- ConsoleApp1/Figure.cs
+++ ConsoleApp1/Figure.cs
@@ -9,31 +9,24 @@ namespace ConsoleApp1
     public class Figure
     {
         private Point[] points;
-
-        public Figure(Point a, Point b, Point c)
-        {
-            points = new Point[3];
-            points[0] = a;
-            points[1] = b;
-            points[2] = c;
-        }
-
-        public Figure(Point a, Point b, Point c, Point d)
-        {
-            points = new Point[4];
-            points[0] = a;
-            points[1] = b;
-            points[2] = c;
-            points[3] = d;
-        }
-
-        public Figure(Point a, Point b, Point c, Point d, Point e)
+        public Figure()
         {
             points = new Point[5];
+        }
+        public Figure(Point a, Point b, Point c) : this()
+        {
             points[0] = a;
             points[1] = b;
             points[2] = c;
+        }
+
+        public Figure(Point a, Point b, Point c, Point d) : this(a,b,c) 
+        {
             points[3] = d;
+        }
+
+        public Figure(Point a, Point b, Point c, Point d, Point e) : this(a,b,c,d)
+        {
             points[4] = e;
         }
         /// строковое автосвойство для хранения названия фигуры
@@ -41,17 +34,19 @@ namespace ConsoleApp1
         /// самые первые скобки - длина по координате, вторые - возводим в квадрат, третьи - квадратный корень из суммы квадратов
         public double LengthSide(Point A, Point B)
         {
-            return Math.Sqrt(Math.Pow((A.X - B.X), 2) + Math.Pow((A.Y - B.Y), 2));
+            return Math.Sqrt(Math.Pow(Math.Abs(A.X - B.X), 2) + Math.Pow(Math.Abs(A.Y - B.Y), 2));
         }
 
         public double PerimeterCalculator()
         {
             double perimeter = 0;
+            int last = 0;
             for (int i = 0; i < points.Length - 1; i++)
             {
-                perimeter += LengthSide(points[i], points[i + 1]);
+                if (points[i] != null && points[i + 1] == null) { last = i; break; }
+                perimeter += LengthSide(points[0], points[i + 1]);
             }
-            perimeter += LengthSide(points[0], points[points.Length - 1]);
+            perimeter += LengthSide(points[0], points[last]);
 
             return perimeter;
         }
